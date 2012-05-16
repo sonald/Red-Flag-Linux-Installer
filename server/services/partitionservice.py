23360ce5f9cf94559a49eac3f9dcbdb7415f66e1
Sian Cao <sycao@redflag-linux.com>
2012-05-16 18:21:59 +0800

    use array instead of tuple for JSON compatible

--- server/services/partitionservice.py
+++ server/services/partitionservice.py
@@ -17,9 +17,9 @@ _stubDisks = [
             "size": "160GB",
             "type": "msdos",
             "table": [
-                (1, "32.3kB", "32.2GB", "32.2GB", "primary", "ext4"),
-                (2, "96.7GB", "160GB", "63.4GB", "extended", ""),
-                (5, "96.7GB", "129GB", "32.2GB", "logical", "ext4"),
+                [ 1, "32.3kB", "32.2GB", "32.2GB", "primary", "ext4" ],
+                [ 2, "96.7GB", "160GB", "63.4GB", "extended", "" ],
+                [ 5, "96.7GB", "129GB", "32.2GB", "logical", "ext4" ],
             ]
         },
         { 
@@ -28,9 +28,9 @@ _stubDisks = [
             "size": "15GB",
             "type": "gpt",
             "table": [
-                (1, "32.3kB", "32.2GB", "32.2GB", "part1", "ext4"),
-                (2, "96.7GB", "160GB", "63.4GB", "part2", ""),
-                (3, "96.7GB", "129GB", "32.2GB", "part3", "ntfs"),
+                [ 1, "32.3kB", "32.2GB", "32.2GB", "part1", "ext4" ],
+                [ 2, "96.7GB", "160GB", "63.4GB", "part2", "" ],
+                [ 3, "96.7GB", "129GB", "32.2GB", "part3", "ntfs" ],
             ]
         },
     ]
