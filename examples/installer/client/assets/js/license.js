a5f58264de6a94f5733a29c17ae18e53f8a7bb36
Sian Cao <sycao@redflag-linux.com>
2012-06-18 19:25:09 +0800

    introdue requirejs to support modules on client side

---
+++ examples/installer/client/assets/js/license.js
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  license.js
+ *
+ *    Description:  license page
+ *
+ *        Version:  1.0
+ *        Created:  2012年06月18日 19时07分32秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sian Cao (sonald), yinshuiboy@gmail.com
+ *        Company:  Red Flag Linux Co. Ltd
+ *
+ * =====================================================================================
+*/
+
+define(['jquery', 'system'], function($) {
+    console.log('load license');
+});
+
---
+++ examples/installer/client/assets/js/system.js
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  system.js
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2012年06月18日 19时06分35秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sian Cao (sonald), yinshuiboy@gmail.com
+ *        Company:  Red Flag Linux Co. Ltd
+ *
+ * =====================================================================================
+*/
+
+require.config({
+    paths: {
+        dnode: '/dnode',
+    },
+    shim: {
+        'bootstrap': ['jquery'],
+    }
+});
+
+// stub to load all hippo system libs
+// the problem is that underscore is actually not AMD-compatible now, it expose global `_`
+// as well as bootstrap
+define(['jquery', 'dnode', 'underscore', 'bootstrap'], function($) {
+    console.log('load sytem');
+    //console.log(_.any);
+});
+
