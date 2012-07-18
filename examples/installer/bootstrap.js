f3666a16a8238a144143ac1ffc0d562e222f77ca
Sian Cao <sycao@redflag-linux.com>
2012-07-18 14:20:05 +0800

    use pkexec as preferred way to launch node

--- examples/installer/bootstrap.js
+++ examples/installer/bootstrap.js
@@ -20,6 +20,8 @@
 // 1. run server in uid == 0
 // 2. try to popup a web browser
 
+/*jslint node: true*/
+
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 var fs = require('fs');
@@ -29,6 +31,9 @@ var pathlib = require('path');
 function testExists(cmd, callback) {
     'use strict';
 
+    if (cmd.indexOf(' ') != -1) {
+        cmd = cmd.split(/\s+/)[0];
+    }
     exec('which ' + cmd, {encoding: 'utf8'}, function(err, stdout) {
         callback(err?false:true);
     });
@@ -36,14 +41,23 @@ function testExists(cmd, callback) {
 
 function startServer() {
     'use strict';
-    async.filter(['gksudo', 'kdesu'], testExists, function(results) {
+
+    var cmd_list = [
+        'pkexec --user root node ' + pathlib.join(__dirname, 'app.js'),
+        'gksudo -t node app.js',
+        'gksu -t node app.js',
+        'kdesu node app.js'
+    ];
+
+    async.filter(cmd_list, testExists, function(results) {
         console.log('found: ', results);
         if (results.length === 0) {
             console.log('no appropriate frontend found');
             process.exit(1);
         }
 
-        var installer = spawn(results[0], ['-t', 'node', 'app.js'], {cwd: __dirname, env: process.env});
+        var cmd = results[0].split(/\s+/);
+        var installer = spawn(cmd[0], cmd.slice(1), {cwd: __dirname, env: process.env});
 
         var fe_loaded = false;
         installer.stdout.on('data', function(data) {
@@ -58,9 +72,10 @@ function startServer() {
         });
 
         process.on('exit', function() {
-            console.log('try kill node');
-            installer.kill('SIGTERM');
-
+            if (!installer.exitCode) {
+                console.log('try kill node');
+                // installer.kill('SIGKILL');
+            }
         });
     });
 }
@@ -76,7 +91,7 @@ function tryLoadFrontend() {
     console.log(options);
 
     var args = [ options.url || 'http://127.0.0.1:8080'];
-    var candidates = [__dirname+ '/libs/run.py', 'chromium', 'google-chrome', 'firefox'];
+    var candidates = ['google-chrome', __dirname+ '/libs/run.py', 'chromium', 'google-chrome', 'firefox'];
 
     async.filter(candidates, testExists, function(results) {
         console.log('found: ', results);
@@ -84,10 +99,12 @@ function tryLoadFrontend() {
             console.log('no appropriate frontend found');
             process.exit(1);
         }
-        var fe = spawn(results[0], [args], {cwd: __dirname, env: process.env} ); 
+        var fe = spawn(results[0], [args], {cwd: __dirname, env: process.env});
 
         process.on('exit', function() {
-            fe.kill('SIGTERM');
+            if (!fe.exitCode) {
+                fe.kill('SIGKILL');
+            }
         });
     });
 }
@@ -105,7 +122,7 @@ function sanityCheck() {
     if (process.getuid() === 0) {
         console.error('please run in user mode');
         process.exit(1);
-    } 
+    }
 
     // check npm dependencies
     if (standalone) {
