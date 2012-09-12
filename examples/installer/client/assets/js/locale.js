2c2202c1e0d34c477df12026b5354713d0828844
Sian Cao <sycao@redflag-linux.com>
2012-09-12 14:56:34 +0800

    fix locale switching problem
    see issue #757

--- examples/installer/client/assets/js/locale.js
+++ examples/installer/client/assets/js/locale.js
@@ -14,7 +14,7 @@
  *        Company:  Red Flag Linux Co. Ltd
  *
  * =====================================================================================
-*/
+ */
 
 /*jslint regexp: true */
 /*global require: false, navigator: false, define: false, console:false */
@@ -23,24 +23,29 @@
     'use strict';
 
     define(['system', 'jed'], {
-	load: function(name, req, load, config) {
-	    console.log('load client locale data');
+        load: function(name, req, load, config) {
+            console.log('load client locale data');
 
-	    var match, lang;
+            var match, lang;
+            var reLocale = /locale=([^=]+)/;
 
-	    if (window.location.search.length > 0) {
-		match = /\?locale=([^=]+)/.exec(window.location.search);
-		if (match) {
-		    lang = match[1];
-	            match = /([^_]+)(?:_([^-]+))?/.exec(lang);
-                    lang = (match && match[1]) || 'en';
-		}
+            if (window.location.search.length > 0) {
+                var queries = window.location.search.replace(/^\?/, '').split('&');
+                for (var i = 0, len = queries.length; i < len; i++) {
+                    if (reLocale.test(queries[i])) {
+                        lang = reLocale.exec(queries[i])[1];
+                        match = /([^_]+)(?:_([^-]+))?/.exec(lang);
+                        console.log('match: %s', match);
+                        lang = (match && match[1]) || 'en';
+                        break;
+                    }
+                }
 
-	    } else {
-		lang = navigator.language || navigator.userLanguage;
-	        match = /([^-]+)(?:-([^-]+))?/.exec(lang);
+            } else {
+                lang = navigator.language || navigator.userLanguage;
+                match = /([^-]+)(?:-([^-]+))?/.exec(lang);
                 lang = (match && match[1]) || 'en';
-	    }
+            }
 
             lang = lang || 'en';
             if (lang === 'POSIX' || lang === 'C') {
@@ -48,15 +53,15 @@
             }
 
             config.locale = lang;
-	    console.log('config: ', config);
-	    req(['jed', '../locales/' + name + '.' + config.locale], function(Jed, value) {
+            console.log('config: ', config);
+            req(['jed', '../locales/' + name + '.' + config.locale], function(Jed, value) {
 
-		//console.log(value);
-		load(new Jed({
-		    'domain': config.locale,
-		    'locale_data': value
-		}));
-	    });
-	}
+                //console.log(value);
+                load(new Jed({
+                    'domain': config.locale,
+                    'locale_data': value
+                }));
+            });
+        }
     });
 }());
